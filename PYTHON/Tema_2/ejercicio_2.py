1372698c8a441b9ef23e0696622439d170e614c6
Marc Pesquera <marc.pesquera@gmail.com>
2022-09-03 04:15:14 +0200

    Añadir ejercicios tema 2 /ejercicio_2

---
+++ PYTHON/Tema_2/ejercicio_2.py
@@ -0,0 +1,12 @@
+# Modifica la variable del anterior ejercicio en la consola de python,
+# y después muestrala por consola para ver la modificación de la variable.
+
+texto = "Esto es un texto"
+
+print(texto)
+
+print(type(texto))
+
+texto = "El texto se ha modificado"
+
+print(texto)
