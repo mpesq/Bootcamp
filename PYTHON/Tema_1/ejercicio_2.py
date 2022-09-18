98c7e84a617f0b2a1d653e68906b555bae5c47c4
Marc Pesquera <109769991+mpesq@users.noreply.github.com>
2022-09-19 01:47:33 +0200

    Delete PYTHON directory

--- PYTHON/Tema_1/ejercicio_2.py
+++
@@ -1,5 +0,0 @@
-# Desde la consola de python almacena la cadena “Hola mundo!” en una variable y muéstrala.
-
-texto = "¡Hola mundo!"
-
-print(texto)
--- PYTHON/Tema_2/ejercicio_1.py
+++
@@ -1,8 +0,0 @@
-# En este ejercicio tendrás que crear una variable a la cual asignarás una cadena de texto y la imprimirás por consola,
-# a su vez, tendrás que imprimir también el tipo de la variable por consola.
-
-texto = "Esto es un texto"
-
-print(texto)
-
-print(type(texto))
