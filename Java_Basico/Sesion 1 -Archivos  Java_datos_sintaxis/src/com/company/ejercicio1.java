d7f86b55875017451130d543509df21399900f38
Marc Pesquera <marc.pesquera@gmail.com>
2022-09-20 19:13:22 +0200

    Ejercicio sesion 1

---
+++ Java_Basico/Sesion 1 -Archivos  Java_datos_sintaxis/src/com/company/ejercicio1.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class ejercicio1 {
+
+    public static void main(String[] args) {
+
+        // Enteros
+        byte num1 = 2;
+        short num2 = 9;
+        int num3 = 14;
+        long num4 = 30;
+
+        System.out.println("1. Datos enteros:");
+        System.out.println("byte: " + num1);
+        System.out.println("short: " + num2);
+        System.out.println("int: " + num3);
+        System.out.println("long: " + num4);
+        System.out.println();
+
+        //decimales
+        float dec1 = 0.89f;
+        double dec2 = 1.14d;
+
+        System.out.println("2. Datos decimales:");
+        System.out.println("float: " + dec1);
+        System.out.println("double: " + dec2);
+        System.out.println();
+
+        //booleano
+        boolean cond = true;
+
+        System.out.println("3. Booleano:");
+        System.out.println("boolean: " + cond);
+        System.out.println();
+
+        //texto
+        char letra = 'g';
+        String texto = "texto";
+
+        System.out.println("4. Texto:");
+        System.out.println("char: " + letra);
+        System.out.println("String: " + texto);
+    }
+}
