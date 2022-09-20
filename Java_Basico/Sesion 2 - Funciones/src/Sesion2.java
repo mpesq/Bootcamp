abd6a893ca92b148b9550c035f5447bc43fbd9c7
Marc Pesquera <marc.pesquera@gmail.com>
2022-09-20 22:02:14 +0200

    Sesión 2 Java

---
+++ Java_Basico/Sesion 2 - Funciones/src/Sesion2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Para este ejercicio tendréis que crear una función que reciba un precio y devuelva el precio con el IVA incluido.
+ */
+
+public class Sesion2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int precio = sc.nextInt();
+        System.out.println(precioIva(precio));
+
+    }
+
+    public static double precioIva(int precio) {
+        return precio * 1.21;
+    }
+}
